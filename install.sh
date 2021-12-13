94a95f368274b261432afe5128b010417d3449ad
Evan Baker <rbtr@users.noreply.github.com>
2021-12-13 21:42:37 +0000

    update
    Signed-off-by: GitHub <noreply@github.com>

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- --yes
 BINDIR=$HOME/bin sh -c "$(curl -fsLS https://raw.githubusercontent.com/twpayne/chezmoi/master/assets/scripts/install.sh)" -- init --apply rbtr
 sudo chsh -s $(which zsh)
