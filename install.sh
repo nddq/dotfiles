83caba868f8f31bdbe2e93716a4b4f74caa46781
Evan Baker <rbtr@users.noreply.github.com>
2021-12-13 21:25:09 +0000

    update
    Signed-off-by: GitHub <noreply@github.com>

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- --yes
 BINDIR=$HOME/bin sh -c "$(curl -fsLS https://raw.githubusercontent.com/twpayne/chezmoi/master/assets/scripts/install.sh)" -- init --apply rbtr
-
+sudo chsh -s $(which zsh)
