9808715c4cedc2ee1d4ce46bb4f8a07c8eb61353
Evan Baker <rbtr@users.noreply.github.com>
2022-12-05 12:53:51 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- --yes
-BINDIR=$HOME/bin sh -c "$(curl -fsLS https://raw.githubusercontent.com/twpayne/chezmoi/master/assets/scripts/install.sh)" -- init --apply rbtr
+sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply rbtr -b $HOME/bin
 sudo chsh -s $(which zsh)
